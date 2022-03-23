66b5732243610d413a701a584f982bb96b7db9f6
owensughrue <owensughrue@college.harvard.edu>
2022-03-22 23:27:07 -0400

    updated week 7

---
+++ 07_electronic_outputs/potentiometer_controlled_led_brightness/potentiometer_controlled_led_brightness.ino
@@ -0,0 +1,21 @@
+/*
+ AnalogRead
+
+*/
+
+int ledPin = 10;
+
+int readValue = 0;
+int ledValue = 0;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  readValue = analogRead(A0);
+  ledValue = map(readValue, 0, 1024, 0, 255);
+  analogWrite(ledPin, ledValue);
+}
