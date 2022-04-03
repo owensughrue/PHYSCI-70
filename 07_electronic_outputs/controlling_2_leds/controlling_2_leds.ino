47ccfee99d2fb3c2c57f6ee44620cf81b861ffc5
owensughrue <owensughrue@college.harvard.edu>
2022-04-03 10:54:53 -0400

    edited files

---
+++ 07_electronic_outputs/controlling_2_leds/controlling_2_leds.ino
@@ -0,0 +1,29 @@
+/
+AnalogRead
+
+/
+
+int ledPin = 10;
+int ledPin2 = 8;
+
+int readValue = 0;
+int ledValue = 0;
+int readValue2 = 0;
+int ledValue2 = 0;
+
+void setup()
+{
+pinMode(ledPin, OUTPUT);
+pinMode(ledPin2, OUTPUT);
+}
+
+void loop()
+{
+readValue = analogRead(A0);
+readValue2 = analogRead(A0);
+
+ledValue = map(readValue, 0, 1024, 0, 255);
+ledValue2 = map(readValue2, 0, 1024, 0, 255);
+analogWrite(ledPin2, ledValue2);
+analogWrite(ledPin, ledValue);
+}
---
+++ 07_electronic_outputs/oscilloscope_code/oscilloscope_code.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(3, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
+
+}
